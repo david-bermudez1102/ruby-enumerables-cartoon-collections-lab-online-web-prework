ca08c3d93edaeac72adab9ba258d801dbd17ad27
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-23 03:37:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def long_planeteer_calls(calls_long)
   sorted_array = calls_long.sort_by { |e|  e.length}
   sorted_array.map do |e|
-    if(e.length > 4) 
+    if(e.length > 4)
       return true
     end
     return false
